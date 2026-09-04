7dda95333691405f4afa40b25bbccd977954504e
chandramapnd <206575909+chandramapnd@users.noreply.github.com>
2026-09-04 18:55:13 +0530

    Time: 2 ms (50.6%), Space: 48.6 MB (20.31%) - LeetHub

---
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
@@ -0,0 +1,32 @@
+
+class Solution {
+    public List<TreeNode> allPossibleFBT(int n) {
+        return solve(n);
+    }
+    public List<TreeNode> solve(int n){
+        if(n%2==0) return (new ArrayList<>());
+
+        if(n == 1){ 
+            List<TreeNode> res = new ArrayList<>(); 
+            res.add(new TreeNode(0));
+            return res;
+        }
+
+        List<TreeNode> res = new ArrayList<>();
+
+        for(int i=1;i<n;i+=2){
+            List<TreeNode> lfbt = solve(i);
+            List<TreeNode> rfbt = solve(n - i -1);
+
+            for(TreeNode l : lfbt){
+                for(TreeNode r : rfbt){
+                    TreeNode root = new TreeNode(0);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
